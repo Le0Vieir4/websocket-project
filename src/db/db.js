585d9997edeff70ca01e488f60c2616101f1dda7
Leo <lvm9508@gmail.com>
2026-04-03 18:46:24 -0300

    implement matches POST and GET routes

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 
 if(!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined in environment variables");
 }
 
-export const pool = new pg.pool({
+export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 })
 
---
+++ src/routes/matches.js
@@ -0,0 +1,47 @@
+import { matches } from "../db/schema.js";
+import { createMatchSchema, listMatchesQuerySchema } from "../validations/matches.js";
+import { db } from "../db/db.js";
+import { Router } from "express";
+import { getMatchStatus } from "../utils/match-status.js";
+import { desc } from "drizzle-orm";
+
+export const matchesRouter = Router()
+
+const MAX_LIMIT = 100;
+
+matchesRouter.get("/", async (req, res) => {
+  const parsed = listMatchesQuerySchema.safeParse(req.query);
+  if(!parsed.success) {
+    return res.status(400).json({ error: 'query parameters are invalid', details: JSON.stringify(parsed.error) })
+  }
+  const limit = Math.min(parsed.data.limit ?? 50, MAX_LIMIT) // set a maximum limit to prevent abuse;
+  try{ 
+    const data = await db.select().from(matches).limit(limit).orderBy((desc(matches.createdAt)));
+    res.json({ data })
+  } catch(error) {
+    return res.status(500).json({ error: 'Failed to retrieve matches', details: JSON.stringify(error) })
+  }
+});
+
+matchesRouter.post("/", async (req, res) => {
+  const parsed = createMatchSchema.safeParse(req.body);
+  if(!parsed.success) {
+    return res.status(400).json({ error: 'payload is invalid', details: JSON.stringify(parsed.error) })
+  }
+  const { startTime, endTime, homeScore, awayScore } = parsed.data
+  try {
+    const [event] = await db.insert(matches).values({
+      ...parsed.data,
+      startTime: new Date(startTime),
+      endTime: new Date(endTime),
+      homeScore: homeScore ?? 0,
+      awayScore: awayScore ?? 0,
+      status: getMatchStatus(startTime, endTime)
+    }).returning()
+    res.status(201).json({ data: event })
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create match', details: JSON.stringify(error) })
+  }
+
+})
+
